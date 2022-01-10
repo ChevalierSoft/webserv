7e0b76000c73f9f58df178849e6e4bbc885be53a
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-10 13:08:21 +0100

    catching SIGINT to clean the memory before the program ends

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <signal.h>			// signal,sigaction
 #include "webserv.hpp"
+
+bool	run = true;
+
+void	sighandler(int signum)
+{
+	// if (signum == SIGINT)
+		run = false;
+}
 
 int main(int argc, char **argv)
 {
 	int	err;
 
-	// TODO (1) parse the config file (default or provided) and feed a list of object with the configurations
+	signal(SIGINT, &sighandler); // making easy to close the program
 
-	// TODO (2) loop over the config_object list and create that many Server objects.
-	Server s(12345);
-	
-	// TODO (3) it could be great to start each Server on a thread
-	err = s.start();
+	if (run)
+	{
+		// TODO (1) parse the config file (default or provided) and feed a list of object with the configurations
+
+		// TODO (2) loop over the config_object list and create that many Server objects.
+		Server s(12345);
+		
+		// TODO (3) it could be great to start each Server on a thread ?
+		err = s.start();
+	}
 
     std::cout << "done " << RED << err <<RST << std::endl;
     return (err);
