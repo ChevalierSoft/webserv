75c4741856be59c3322f4477155d66e01d8d04e0
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-12 06:41:05 +0100

    added a test container that starts a webserver (apache2). cgi_just_print.sh is accessible via http://localhost:4040/cgi-bin/cgi_just_print.sh

--- tst/cgi/cgi_just_print.sh
+++ tst/cgi/cgi_just_print.sh
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    cgi_just_print.sh                                  :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/01/12 04:12:05 by dait-atm          #+#    #+#              #
-#    Updated: 2022/01/12 04:13:54 by dait-atm         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/bash
 
 echo "Content-type: text/html"
@@ -21,7 +9,7 @@ echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
 echo '<title>static html sent from cgi_just_print.sh</title>'
 echo '</head>'
 echo '<body>'
-echo 'static html sent from cgi_just_print.sh'
+echo '<h3>static html sent from cgi_just_print.sh</h3>'
 echo '</body>'
 echo '</html>'
 
