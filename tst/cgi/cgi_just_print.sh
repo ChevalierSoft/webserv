e3eb75344cad26870d30f209fdfd5ffd29ae7fce
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-12 07:13:23 +0100

    patched form in index.html and pimped cgi_just_print.sh

--- tst/cgi/cgi_just_print.sh
+++ tst/cgi/cgi_just_print.sh
@@ -10,6 +10,7 @@ echo '<title>static html sent from cgi_just_print.sh</title>'
 echo '</head>'
 echo '<body>'
 echo '<h3>static html sent from cgi_just_print.sh</h3>'
+echo '<img src="https://media.tenor.com/images/01b9dd00df8b96b64600932543dfce72/tenor.gif"/>'
 echo '</body>'
 echo '</html>'
 
