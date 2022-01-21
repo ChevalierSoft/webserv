4ea2ec5e33bf50164260ec003cfcc448214ec486
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-21 17:43:38 +0100

    added utils.cpp/hpp that will be a compilation of usefull functions that are rewrited from c++11 or that are usefull here and there in the program.

---
+++ includes/utils.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 17:07:53 by dait-atm          #+#    #+#             */
+/*   Updated: 2022/01/21 17:21:45 by dait-atm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <string>
+
+std::string get_file_name(const std::string & path);
+std::string	get_flie_extention(const std::string & file_name);
