52d8d40cea3cddf48bc7f0672d19538b20176a56
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-21 11:12:00 +0100

    now the server is set with the Conf provided in the main. an address to this Conf is shared with the clients.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,24 +11,25 @@ void	sighandler(int signum)
 
 int main(int argc, char **argv)
 {
-	int	err;
+	int	err = 0;
 
 	signal(SIGINT, &sighandler);	// making it easy to close the program
 	signal(SIGQUIT, &sighandler);
 	
+	// // TODO (1) parse the config file (default or provided) and feed a list of object with the configurations
 	Parser	p("tst/conf/webserv.conf");
 	run = !p._err;
 	// for (conf_list::iterator it = p._confs.begin(); it != p._confs.end(); it++)
 	// 	(*it).print();
 	if (run == false)
+	{
 		std::cerr << "Error: " << p._error_message << std::endl;
-
+		return (20);
+	}
 	else
 	{
-		// TODO (1) parse the config file (default or provided) and feed a list of object with the configurations
-
 		// TODO (2) loop over the config_object list and create that many Server objects.
-		Server s(12345);
+		Server s(*(p._confs.begin()));
 		
 		// TODO (3) it could be great to start each Server on a thread ?
 		err = s.start();
