b0499065c73d977753a645359d1066484f1e891a
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-22 09:58:56 +0100

    now content-types are read from resources/content-types.webserv and stored. TODO: change the rights to this files to avoid unwanted changes from the user.

--- srcs/set_content_types.cpp
+++ srcs/set_content_types.cpp
@@ -6,28 +6,39 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 08:19:54 by dait-atm          #+#    #+#             */
-/*   Updated: 2022/01/22 09:07:23 by dait-atm         ###   ########.fr       */
+/*   Updated: 2022/01/22 09:53:29 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <map>
 #include <string>
+#include <fstream>
+#include <iostream> // debug
 
+#define REFERENCE_CONTENT_TYPE "./resources/content-types.webserv"
+
+/**
+ * @brief Set a map with commun content types
+ * @details Commun content types where found here
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+ * @return std::map<std::string, std::string> 
+ */
 std::map<std::string, std::string>	set_content_types()
 {
-	std::map<std::string, std::string> ct;
+	std::map<std::string, std::string>	ct;
+	std::string							line;
+	std::ifstream						ifs(REFERENCE_CONTENT_TYPE);
+	std::string							key, value;
 
-	// ? application
-	
-	// ? text
-	ct.insert(std::make_pair(".html", "text/html"));
-	ct.insert(std::make_pair(".css", "text/css"));
+	if  (ifs.good())
+	{
+		while (getline(ifs, line))
+		{
+			ifs >> key >> value;
+			// //std::cout << key << " " << value << std::endl;
+			ct.insert(std::make_pair(key, value));
+		}
+	}
 
-	// ? image
-	ct.insert(std::make_pair(".gif", "image/gif"));
-
-	// ? video
-
-	
 	return (ct);
 }
