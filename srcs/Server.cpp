bcc8ca57b153102c3453b0595f2c4e0248590121
ChevalierSoft <ibanarashi@outlook.fr>
2022-01-24 18:05:14 +0100

    patched the bytes order while getting port number.

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 06:25:14 by dait-atm          #+#    #+#             */
-/*   Updated: 2022/01/24 17:17:29 by dait-atm         ###   ########.fr       */
+/*   Updated: 2022/01/24 18:04:32 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,8 +164,7 @@ bool			Server::add_new_client ()
 	new_sd = accept(_listen_sd, (struct sockaddr *)&addr, &addr_size);
 
 	std::cout << "  New connection from : " << inet_ntoa(((addr)).sin_addr) << std::endl;
-	std::cout << "  on port : " << (addr).sin_port << std::endl;
-	// std::cout << "on port : " << htons((addr).sin_port) << std::endl;
+	std::cout << "  on port : " << htons((addr).sin_port) << std::endl;
 
 	if (new_sd < 0)
 	{
@@ -180,7 +179,7 @@ bool			Server::add_new_client ()
 	tmp.revents = 0;
 	_fds.push_back(tmp);
 
-	_clients[new_sd] = Client(&this->_conf, inet_ntoa((addr).sin_addr), ft_to_string(addr.sin_port));
+	_clients[new_sd] = Client(&this->_conf, inet_ntoa((addr).sin_addr), ft_to_string(htons((addr).sin_port));
 
 	return (true);
 }
