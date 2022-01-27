ce5cbaa69ddb37c4799a70124b3287c1cd25abb2
Waluiji <ljurdant@student.42.fr>
2022-01-27 21:10:26 +0100

    conf descriptions added and hot:port parsing changed

--- srcs/Conf.cpp
+++ srcs/Conf.cpp
@@ -9,8 +9,7 @@
 #include <cctype>
 
 Conf::Conf(): _name(name_type()),
-_host(host_type()),
-_port(port_type()),
+_hosts(host_list()),
 _client_body_size(size_t()),
 _methods(method_list()),
 _dir_listing(-1),
@@ -39,8 +38,7 @@ Conf::~Conf() {}
 
 Conf	&Conf::operator=(Conf const &rhs) {
 	_name = rhs._name;
-	_host = rhs._host;
-	_port = rhs._port;
+	_hosts = rhs._hosts;
 	_error_pages = rhs._error_pages;
 	_client_body_size = rhs._client_body_size;
 	_methods = rhs._methods;
@@ -74,10 +72,41 @@ int	my_strtoi(std::string s)
 	return (n);
 }
 
-Conf::port_type   Conf::string_to_port(std::string value) {
+Conf::port_type	Conf::string_to_port(std::string value) {
 	if (value == "" || count_if(value.begin(), value.end(), isnotdigit))
 		return (-1);
 	return (my_strtoi(value));
+}
+
+Conf::host_pair	Conf::string_to_host_pair(std::string value) {
+	size_t	found;
+	const char	sep = ':';
+	host_pair	host;
+
+	found = value.find(sep);
+	host.first = value.substr(0, found);
+	found++;
+	if (found != std::string::npos)
+		host.second = string_to_port(value.substr(found, value.size() - found));
+	else
+		host.second = -1;
+	return (host);
+}
+
+Conf::host_list	Conf::string_to_hosts(std::string value) {
+	const char	sep = ',';
+	size_t		start = 0;
+	size_t		end;
+	host_list	hosts;
+
+	while (start <= value.size())
+	{
+		if ((end = value.find(sep, start)) == std::string::npos)
+			end = value.size();
+		hosts.push_back(string_to_host_pair(value.substr(start, end - start)));
+		start = ++end;
+	}
+	return (hosts);
 }
 
 Conf::code_type     Conf::string_to_code(std::string value) {
@@ -151,21 +180,28 @@ Conf::dir_listing_type	Conf::string_to_dir_listing(std::string value) {
 bool    Conf::set_name(name_type name) {
 	if (name == "")
 		return (set_error_message("Invalid value: name"));
+	else if (_name != name_type())
+		return (set_error_message("Double parameter: name"));
 	_name = name;
 	return (true);
 }
 
-bool    Conf::set_host(host_type host) {
-	if (host == "")
+bool	Conf::add_host(host_pair host) {
+	for (host_list::iterator it = _hosts.begin(); it != _hosts.end(); it++)
+		if (*it == host)
+			return (set_error_message("Double parameter: host"));
+	if (host.first == "" || host.second == -1)
 		return (set_error_message("Invalid value: host"));
-	_host = host;
+	_hosts.push_back(host);
 	return (true);
 }
 
-bool        Conf::set_port(port_type port) {
-	if (port < 0)
-		return (set_error_message("Invalid value: port"));
-	_port = port;
+bool	Conf::set_hosts(host_list hosts) {
+	if (_hosts != host_list())
+		return (set_error_message("Double parameter: hosts"));
+	for (host_list::iterator it = hosts.begin(); it != hosts.end(); it++)
+		if (!add_host(*it))
+			return (false);
 	return (true);
 }
 
@@ -179,12 +215,16 @@ bool    Conf::add_error(error_type  error) {
 bool	Conf::set_client_body_size(size_type client_body_size) {
 	if (client_body_size < 0)
 		return (set_error_message("Invalid value: client_body_size"));
+	else if (_client_body_size != size_type())
+		return (set_error_message("Double parameter: client_body_size"));
 	_client_body_size = client_body_size;
 	return (true);
 }
 
 bool	Conf::add_method(method_type method) {
-	
+	for (method_list::iterator it = _methods.begin(); it != _methods.end(); it++)
+		if (*it == method)
+			return (set_error_message("Double parameter: method"));
 	if (!(method == "GET" || method == "POST" || method == "DELETE"))
 		return (set_error_message("Invalid value: methods"));
 	_methods.push_back(method);
@@ -192,6 +232,8 @@ bool	Conf::add_method(method_type method) {
 }
 
 bool	Conf::set_methods(method_list methods) {
+	if (_methods != method_list())
+		return (set_error_message("Double parameter: methods"));
 	for (method_list::iterator it = methods.begin(); it != methods.end(); it++)
 		if (!add_method(*it))
 			return (false);
@@ -214,20 +256,27 @@ bool	Conf::set_cgi(cgi_list cgi) {
 }
 
 bool	Conf::set_dir_listing(dir_listing_type dir_listing) {
-	if (dir_listing < 0)
+	if (_dir_listing != -1)
+		return (set_error_message("Double parameter: directory_listing"));
+	else if (dir_listing < 0)
 		return (set_error_message("Invalid value: directory_listing"));
 	_dir_listing = dir_listing;
 	return (true);
 }
 
 bool	Conf::set_upload_path(path_type upload_path) {
-	if (upload_path == "")
-		return(set_error_message("Invalid value: uploads"));
+	if (_upload_path != path_type())
+		return (set_error_message("Double parameter: uploads"));
+	else if (upload_path == "")
+		return (set_error_message("Invalid value: uploads"));
 	_upload_path = upload_path;
 	return (true);
 }
 
 bool	Conf::add_route(route_type route) {
+	for (route_list::iterator it = _routes.begin(); it != _routes.end(); it++)
+		if (it->_path == route._path)
+			return (set_error_message(route._path+": already exists"));
 	if (route._err)
 		return (set_error_message(route._error_message));
 	_routes.push_back(route);
@@ -236,9 +285,16 @@ bool	Conf::add_route(route_type route) {
 
 void	Conf::print() {
 	std::cout << "name = " << _name << std::endl;
-	std::cout << "host = " << _host << std::endl;
-	std::cout << "port = " << _port << std::endl;
-	std::cout << "error_pages:" << std::endl;
+	std::cout << "hosts = ";
+	for (host_list::iterator it = _hosts.begin(); it != _hosts.end(); it++)
+	{
+		if (it != _hosts.begin())
+			std::cout << ", ";
+		std::cout << it->first << ":" << it->second;
+	}
+	std::cout << std::endl;
+	if (_error_pages != error_list())
+		std::cout << "error_pages:" << std::endl;
 	for (error_list::iterator it = _error_pages.begin(); it != _error_pages.end(); it++)
 		std::cout << "\t" << (*it).first << ": " << (*it).second << std::endl;
 	std::cout << "client_body_size = " << _client_body_size << std::endl;
@@ -249,12 +305,18 @@ void	Conf::print() {
 			std::cout << ", ";
 		std::cout << *it;
 	}
-	std::cout << std::endl;
-	std::cout << "directory listing = " << _dir_listing << std::endl;
-	std::cout << "uploads = " << _upload_path << std::endl;
+	if (_error_pages != error_list())
+		std::cout << std::endl;
+	if (_dir_listing != -1)
+		std::cout << "directory listing = " << _dir_listing << std::endl;
+	if (_upload_path != path_type())
+		std::cout << "uploads = " << _upload_path << std::endl;
 	std::cout << "routes:" << std::endl;
 	for (route_list::iterator it = _routes.begin(); it != _routes.end(); it++)
+	{
 		(*it).print();
+		std::cout << std::endl;
+	}
 }
 
 bool		Conf::set_error_message(std::string error_message) {
@@ -266,14 +328,27 @@ bool		Conf::set_error_message(std::string error_message) {
 bool		Conf::check() {
 	if (_name == name_type())
 		return (set_error_message("Required value: server_name"));
-	else if (_host == host_type())
+	else if (_hosts == host_list())
 		return (set_error_message("Required value: host"));
-	else if (_port == port_type())
-		return (set_error_message("Required value: port"));
 	else if (_client_body_size == size_type())
 		return (set_error_message("Required value: client_body_size"));
+	else if (_routes == route_list())
+		return (set_error_message("Required value: routes"));
 	for (route_list::iterator it = _routes.begin(); it != _routes.end(); it++)
+	{
 		if (!it->check())
 			return (set_error_message(it->_error_message));
+		/*Recursive redirection check*/
+		if (it->_redir != route_type::redir_type())
+		{
+			for (route_list::iterator it2 = _routes.begin(); it2 != _routes.end(); it2++)
+			{
+				if (it2->_path == it->_redir.second && it2->_redir != route_type::redir_type()\
+				 && it2->_redir.second == it->_path)
+				 	return (set_error_message("Redirections: Recursive loop"));
+
+			}
+		}
+	}
 	return (true);
 }
--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -115,10 +115,8 @@ bool	Parser::zero_indent(param_type param, std::string value)
 
 	if (param == "server_name")
 		return (conf.set_name(value));
-	else if (param == "host")
-		return (conf.set_host(value));
-	else if (param == "port")
-		return (conf.set_port(conf.string_to_port(value)));
+	else if (param == "hosts")
+		return (conf.set_hosts(conf.string_to_hosts(value)));
 	else if (param == "client_body_size")
 		return (conf.set_client_body_size(conf.string_to_client_body_size(value)));
 	else if (param == "methods")
@@ -164,7 +162,7 @@ bool	Parser::two_indent(param_list params, std::string value)
 			return (route.set_default_file(value));
 		else if (params[2] == "uploads")
 			return (route.set_upload_path(value));
-		else if (params[2] == "redirections")
+		else if (params[2] == "redirection")
 			return (true);
 		else if (params[2] == "cgi")
 			return (route.set_cgi(conf.string_to_cgi(value)));
@@ -178,7 +176,7 @@ bool	Parser::three_indent(param_list params, std::string value) {
 	if (params[0] == "routes")
 	{
 		Route	&route = conf._routes.back();
-		if (params[2] == "redirections")
+		if (params[2] == "redirection")
 			return (route.set_redir(std::make_pair(conf.string_to_code(params[3]), value)));
 	}
 	return (set_error_message("Invalid parameter"));
@@ -201,9 +199,17 @@ bool		Parser::set_error_message(std::string error_message) {
 	return (false);
 }
 
+
 bool	Parser::check() {
 	for (conf_list::iterator it = _confs.begin(); it != _confs.end(); it++)
+	{
 		if (!it->check())
 			return (set_error_message(it->_error_message));
+	}
 	return (true);
+}
+
+void	Parser::print() {
+	for (conf_list::iterator it = _confs.begin(); it != _confs.end(); it++)
+		it->print();
 }
